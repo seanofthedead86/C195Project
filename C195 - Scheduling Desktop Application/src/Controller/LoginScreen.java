10152a32b30bdfa6364460e0c9fe89e6d3015fa3
Sean Cooper <35088180+seanofthedead86@users.noreply.github.com>
2020-11-16 19:14:59 -0500

    Add files via upload

---
+++ C195 - Scheduling Desktop Application/src/Controller/LoginScreen.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginScreen {
+
+    @FXML
+    private TextField usernameInput;
+
+    @FXML
+    private TextField passwordInput;
+
+    @FXML
+    private Label locationHereText;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    void submitButton(ActionEvent event) {
+
+    }
+}
